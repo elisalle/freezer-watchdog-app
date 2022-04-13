bc1da1e05a000a909286eca5317e3c5fd0871828
Eli Salle <esalle737@gmail.com>
2022-04-13 15:37:27 +0200

    add functionality to back button, correct string

--- app/src/main/java/com/example/freezerwatchdog/SettingsActivity.kt
+++ app/src/main/java/com/example/freezerwatchdog/SettingsActivity.kt
@@ -1,18 +1,36 @@
 package com.example.freezerwatchdog
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
 
 class SettingsActivity : AppCompatActivity() {
+    val LOG_TAG = "SettingsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar);
+        setSupportActionBar(toolbar)
+
+
+        toolbar.setNavigationOnClickListener(View.OnClickListener() {
+            // back button pressed
+            onBackPressed()
+        });
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
